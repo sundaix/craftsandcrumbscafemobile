d8f2c4eee5eb510275ce2f9dd83bc7045c3961bf
Karl <94129669+sundaix@users.noreply.github.com>
2026-09-07 19:39:17 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,3 +1,3 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = { appId: 'com.craftsandcrumbs.app', appName: 'Crafts & Crumbs', webDir: 'www' };
+const config: CapacitorConfig = { appId: 'com.craftsandcrumbs.app', appName: 'Crafts & Crumbs', webDir: 'www/browser' };
 export default config;
